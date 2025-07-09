cf940f7ede2cf6169dbe4bc4d94a394170984092
alinsran <alinsran@apache.org>
2025-07-10 03:12:46 +0800

    update test

--- test/e2e/scaffold/assertion.go
+++ test/e2e/scaffold/assertion.go
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package scaffold
 
 import (
@@ -24,14 +41,21 @@ type HTTPResponse struct {
 	Body string
 }
 
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
 type RequestAssert struct {
-	Client   *httpexpect.Expect
-	Method   string
-	Path     string
-	Host     string
-	Query    map[string]any
-	Headers  map[string]string
-	Body     []byte
+	Client    *httpexpect.Expect
+	Method    string
+	Path      string
+	Host      string
+	Query     map[string]any
+	Headers   map[string]string
+	Body      []byte
+	BasicAuth *BasicAuth
+
 	Timeout  time.Duration
 	Interval time.Duration
 
@@ -145,6 +169,9 @@ func (s *Scaffold) RequestAssert(r *RequestAssert) bool {
 			for key, value := range r.Query {
 				req = req.WithQuery(key, value)
 			}
+		}
+		if r.BasicAuth != nil {
+			req = req.WithBasicAuth(r.BasicAuth.Username, r.BasicAuth.Password)
 		}
 		expResp := req.Expect()
 
